01b1d840016549e9d9881f3c32b34a3b4b0de393
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-04-14 18:18:03 +0200

    seymas branch

---
+++ client/src/containers/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+/* import React from "react";
+import { useForm } from "react-hook-form";
+
+import { connect, useDispatch } from "react-redux";
+import { createSession } from "../../reducers/actions/index";
+
+function Dashboard() {}
+
+function mapState(state) {
+  return {
+    messageStore: state.authorization.message,
+  };
+}
+
+export default connect(mapState)(Dashboard);
+ */
