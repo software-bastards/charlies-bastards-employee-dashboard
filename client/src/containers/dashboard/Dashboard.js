a75158ec956c58918adba5498039ef4e268f8010
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-04-14 18:25:38 +0200

    dashboard client

---
+++ client/src/containers/dashboard/Dashboard.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import dashboardHelper from "../../services/dashboardHelper";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return {
+    token: state.authorization.token,
+    firstname: state.authorization.firstname,
+  };
+}
+class Dashboard extends Component {
+  state = {
+    account: {
+      firstname: "",
+      lastname: "",
+    },
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.token !== prevProps.token) {
+      this.getAccountData(this.props.token);
+    }
+  }
+  getAccountData = (props) => {
+    dashboardHelper(props)
+      .then((res) =>
+        this.setState({
+          account: {
+            firstname: res.data.firstname,
+            lastname: res.data.lastname,
+          },
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="dash-container">
+        <h1>Welcome , {this.state.account.firstname} </h1>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
