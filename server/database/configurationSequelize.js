2d763ffa76a2b2347033000a257dc9347808f00c
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-04-15 12:04:42 +0200

    database config in local

--- server/database/configurationSequelize.js
+++ server/database/configurationSequelize.js
@@ -1,47 +1,58 @@
-const enVars = require ("./envVariables")
-const Sequelize = require('sequelize');
+const enVars = require("./envVariables");
+const Sequelize = require("sequelize");
 
-
-const connector = new Sequelize(  enVars.development.database,  enVars.development.user,  enVars.development.password,     {
+const connector = new Sequelize(
+  enVars.development.database,
+  enVars.development.user,
+  enVars.development.password,
+  {
     host: enVars.development.host,
     dialect: enVars.development.dialect,
-    define:{
-        timestamps:false
-    }
-     });
-  
-  const authenticate = async connector =>{
-      try{
-          await connector.authenticate();
-          console.log(`connection to db was good`);
-      }
-      catch(e){
-          console.error(e)
-      }
+    define: {
+      timestamps: false,
+    },
   }
+);
 
-  authenticate(connector);
+const authenticate = async (connector) => {
+  try {
+    await connector.authenticate();
+    console.log(`connection to db was good`);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
- const db ={ }
+authenticate(connector);
 
-db.Sequelize = Sequelize
-db.connector=connector 
-db.account = require("../models/account.model")(Sequelize,connector)
-db.upload = require("../models/upload.model")(Sequelize,connector)
-db.project = require("../models/project.model")(Sequelize,connector)
-db.hour = require("../models/hour.model")(Sequelize,connector)
-db.category = require("../models/category.model")(Sequelize,connector)
+const db = {};
 
-//upload conection 
-db.upload.belongsTo(db.account, {foreignKey: 'id', targetKey: 'upload_id'});
+db.Sequelize = Sequelize;
+db.connector = connector;
+db.account = require("../models/account.model")(Sequelize, connector);
+db.upload = require("../models/upload.model")(Sequelize, connector);
+db.project = require("../models/project.model")(Sequelize, connector);
+db.hour = require("../models/hour.model")(Sequelize, connector);
+db.category = require("../models/category.model")(Sequelize, connector);
 
-//account conection 
-db.account.hasMany(db.project, {foreignKey: 'account_id', sourceKey: 'id'}) 
-db.account.hasMany(db.hour, {foreignKey: 'account_id', sourceKey: 'id'}) 
+//upload conection
+db.upload.belongsTo(
+  db.account /* , {foreignKey: 'id', targetKey: 'upload_id'} */
+);
+
+//account conection
+
+db.project.belongsTo(db.account);
+db.account.hasMany(
+  db.project /* , { foreignKey: "account_id", sourceKey: "id" } */
+);
+db.hour.belongsTo(db.account);
+db.account.hasMany(
+  db.hour /* , { foreignKey: "account_id", sourceKey: "id" } */
+);
 
 // hour conection
-db.hour.belongsTo(db.category) 
-db.hour.hasOne(db.upload)
+db.hour.belongsTo(db.category);lsof -i:5000
+db.hour.hasOne(db.upload);
 
-
- module.exports = db
+module.exports = db;
--- server/models/upload.model.js
+++ server/models/upload.model.js
@@ -1,23 +1,20 @@
-module.exports= (Sequelize,connector) =>{
-    const Upload = connector.define('upload',  {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-          upload_name:{
-              type:Sequelize.STRING,
-          },
-          createdAt: Sequelize.DATE,
-          updatedAt: Sequelize.DATE
-            },
-            {underscored: true,
-              freezeTableName: true
-              
-              })
-              
-                 
+module.exports = (Sequelize, connector) => {
+  const Upload = connector.define(
+    "upload",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      upload_name: {
+        type: Sequelize.STRING,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    },
+    { underscored: true, freezeTableName: true }
+  );
 
-    return Upload}
-   
-     
+  return Upload;
+};
