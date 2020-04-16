ec8123d669ede3d7d34dd4cf27552f88f28a3320
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-04-17 00:14:04 +0200

    dashboard logout trial

--- client/src/containers/dashboard/Dashboard.js
+++ client/src/containers/dashboard/Dashboard.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
 import dashboardHelper from "../../services/dashboardHelper";
 import { connect } from "react-redux";
+/* import { logoutUser } from "../../reducers/actions/logoutUser"; */
+import { useHistory } from "react-router-dom";
 
-function Dashboard({ token, firstname }) {
+function Dashboard({ token, firstname, lastname }) {
   const [user, setUser] = useState("");
 
-  useEffect((prevProps) => {
+  /*   useEffect((prevProps) => {
     console.log(firstname);
-  });
+  }); */
+  /*   const logOut = (e) => {
+    e.preventDefault();
+    setUser(!isAuthenticated);
+  }; */
 
+  const history = useHistory();
+
+  const onLogOut = (e) => {
+    localStorage.removeItem("token");
+    history.push("/");
+    /*       .catch((err) => setMessage(`${err.response.data.message.message}`));
+     */
+  };
   return (
     <div className="dash-container">
-      <h1>Welcome {firstname} </h1>
+      <h1>
+        Welcome {firstname} {lastname}
+      </h1>
+      <button onClick={() => onLogOut()}> Log Out</button>
     </div>
   );
 }
@@ -20,6 +37,8 @@ function mapStateToProps(state) {
   return {
     token: state.authorization.token,
     firstname: state.authorization.firstname,
+    lastname: state.authorization.lastname,
+    isAuthenticated: state.authorization.token,
   };
 }
 
