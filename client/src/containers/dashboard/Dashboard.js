aa02d9ce03744e3789beec06f582936c390cc04a
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-04-19 14:24:21 +0200

    lol

--- client/src/containers/dashboard/Dashboard.js
+++ client/src/containers/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import dashboardHelper from "../../services/dashboardHelper";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 /* import { logoutUser } from "../../reducers/actions/logoutUser"; */
 import { useHistory } from "react-router-dom";
 
 function Dashboard({ token, firstname, lastname }) {
   const [user, setUser] = useState("");
+  const history = useHistory();
+  const authorization = useSelector((store) => {
+    return store.authorization;
+  });
 
   /*   useEffect((prevProps) => {
     console.log(firstname);
@@ -15,31 +19,29 @@ function Dashboard({ token, firstname, lastname }) {
     setUser(!isAuthenticated);
   }; */
 
-  const history = useHistory();
-
-  const onLogOut = (e) => {
+  /*   const onLogOut = (e) => {
     localStorage.removeItem("token");
     history.push("/");
     /*       .catch((err) => setMessage(`${err.response.data.message.message}`));
      */
-  };
+
   return (
     <div className="dash-container">
       <h1>
-        Welcome {firstname} {lastname}
+        Welcome {authorization.firstname} {authorization.lastname}
       </h1>
-      <button onClick={() => onLogOut()}> Log Out</button>
+      <button /* onClick={() => onLogOut()} */> Log Out</button>
     </div>
   );
 }
 
-function mapStateToProps(state) {
+/* function mapStateToProps(state) {
   return {
     token: state.authorization.token,
     firstname: state.authorization.firstname,
     lastname: state.authorization.lastname,
     isAuthenticated: state.authorization.token,
   };
-}
+} */
 
-export default connect(mapStateToProps)(Dashboard);
+export default Dashboard;
--- client/src/reducers/authorization.js
+++ client/src/reducers/authorization.js
@@ -4,10 +4,17 @@
  * @param {object} action - action to be reduce
  */
 
-export default function authorization(state={}, action){
-switch(action.type){
+export default function authorization(state = {}, action) {
+  switch (action.type) {
     case "CREATE_SESSION":
-    return {...state, token:action.token, firstname:action.firstname,lastname:action.lastname,message:action.message}
-    default: 
-    return state;
-}}
+      return {
+        ...state,
+        token: action.token,
+        firstname: action.firstname,
+        lastname: action.lastname,
+        message: action.message,
+      };
+    default:
+      return state;
+  }
+}
