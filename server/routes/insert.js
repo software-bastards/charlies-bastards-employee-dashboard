8ae397c35f6a55d51c00673776bd938b8b16fd15
Ligita <legyta@gmail.com>
2020-04-22 16:00:51 +0200

    jwt authent added

--- server/routes/insert.js
+++ server/routes/insert.js
@@ -7,16 +7,28 @@ const db = require("../database/configurationSequelize");
 const hour = db.hour;
 const account = db.account;
 
-router.post("/inserthours", (req, res, next) => {
-  hour
-    .create({
-      hour: req.body.hour,
-      mounth_number: req.body.mounth_number,
-      day_number: req.body.day_number,
-      account_id: req.body.account_id,
-    })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
-});
+//need to add jwt token
+
+router.post(
+  "/inserthours",
+  (req, res, next) => {
+    passport.authenticate("jwt", { session: false }, (err, user, infor) => {
+      if (err) console.log(err);
+    });
+    next();
+  },
+
+  (req, res) => {
+    hour
+      .create({
+        hour: req.body.hour,
+        mounth_number: req.body.mounth_number,
+        day_number: req.body.day_number,
+        account_id: req.body.account_id,
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+);
 
 module.exports = router;
