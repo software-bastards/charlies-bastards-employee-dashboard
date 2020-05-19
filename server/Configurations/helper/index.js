e65d8b4354a3ba2cd8598902728ab3aca3acbf6a
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-05-19 16:14:11 +0200

    flags added

---
+++ server/Configurations/helper/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // isEmpty: (object) => {
+  //   for (const key in object) {
+  //     if (object.hasOwnProperty(key)) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // },
+  // isObjectPropertyEmpty: (object) =>
+  //   Object.values(object).some((key) => key === null || key === ""),
+  isNotNumber: (incomingId) => isNaN(incomingId),
+};
--- server/routes/insert.js
+++ server/routes/insert.js
@@ -7,26 +7,39 @@ const db = require("../database/configurationSequelize");
 const hour = db.hour;
 const account = db.account;
 const passport = require("passport");
-
+//Helper functions
+const { isNotNumber } = require("../Configurations/helper");
 router.post(
   "/inserthours",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    hour
-      .create({
-        hour_logged: req.body.hour,
-        month_number: req.body.month_number,
-        day_number: req.body.day_number,
-        account_id: req.body.account_id,
-      })
-      .then((response) => res.status(200).send(response.dataValues))
-      .catch((error) =>
-        res.status(500).send({
-          message:
-            "Sorry! We are currently having server difficulties. Try again later",
-        })
-      );
+    //check if entered value is number
+    if (isNotNumber(req.body.mounth_number)) {
+      res.status(400).send({ message: "Oops, month should be a number!" });
+    } else {
+      if (isNotNumber(req.body.hour)) {
+        res.status(400).send({ message: "Oops, hour should be a number!" });
+      } else {
+        if (isNotNumber(req.body.day_number)) {
+          res.status(400).send({ message: "Oops, day should be a number!" });
+        } else {
+          hour
+            .create({
+              hour: req.body.hour,
+              mounth_number: req.body.mounth_number,
+              day_number: req.body.day_number,
+              account_id: req.body.account_id,
+            })
+            .then((response) => res.status(200).send(response.dataValues))
+            .catch((error) =>
+              res.status(500).send({
+                message:
+                  "Sorry! We are currently having server difficulties. Try again later",
+              })
+            );
+        }
+      }
+    }
   }
 );
-
 module.exports = router;
